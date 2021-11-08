5d546714bade066fb5d186bdd1691817bc957e58
Phalanaxis <59425392+Phalanaxis@users.noreply.github.com>
2021-11-08 20:31:17 +0400

    Additional Tasks Commit

---
+++ JavaScript_TestProject/js_scripts/InJSON.js
@@ -0,0 +1,7 @@
+let user = {
+    name: "Василий Иванович",
+    age: 35,
+};
+
+let result = JSON.stringify(user);
+alert(result);
---
+++ JavaScript_TestProject/js_scripts/menu.js
@@ -0,0 +1,8 @@
+result = prompt("Введите код апускаемого скрипта", 0);
+
+switch (result){
+    case 1:
+        document.createElement(<script src = "Filtration.js"></script>);
+    default:
+        break;
+}
